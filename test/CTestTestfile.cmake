e595ac365f022ccf8b9923dd80abfa02b0a9a5e4
James Cuzella <james.cuzella@lyraphase.com>
2022-01-27 00:32:29 -0700

    Refactor multiple CTest properties into single set_tests_properties call

--- test/CTestTestfile.cmake
+++ test/CTestTestfile.cmake
@@ -2,15 +2,15 @@ add_test (NAME inputOutputTest WORKING_DIRECTORY ${helloworld_BINARY_DIR} COMMAN
 
 set (inputOutputPassPromptRegex "^Enter a value: " "^You entered: abcd:12:34:99999999999999.000000$" "^Items read: 4$")
 set (inputOutputFailRegex "Error" "Fail" "^Abort trap: \\d+")
-set_property (TEST inputOutputTest
-              PROPERTY PASS_REGULAR_EXPRESSION "${inputOutputPassPromptRegex}")
-set_property (TEST inputOutputTest
-              PROPERTY FAIL_REGULAR_EXPRESSION "${inputOutputFailRegex}")
+set_tests_properties (inputOutputTest PROPERTIES
+              PASS_REGULAR_EXPRESSION "${inputOutputPassPromptRegex}"
+              FAIL_REGULAR_EXPRESSION "${inputOutputFailRegex}"
+              REQUIRED_FILES ${helloworld_SOURCE_DIR}/test/fixtures/inputOutputTest.txt)
 
 add_test (NAME largeInputTest WORKING_DIRECTORY ${helloworld_BINARY_DIR} COMMAND sh -c "./helloworld < ${helloworld_SOURCE_DIR}/test/fixtures/largeInputTest.txt")
-set (largeInputPassPromptRegex "^Enter a value: " "^You entered: abcdefghijklmnopqrstuvwxyz01234567890abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789000:12:34:99999999999999.000000$" "^Items read: 4$")
-set (largeInputFailRegex "Error" "Fail" "^Abort trap: \\d+")
-set_property (TEST largeInputTest
-              PROPERTY PASS_REGULAR_EXPRESSION "${largeInputPassPromptRegex}")
-set_property (TEST largeInputTest
-              PROPERTY FAIL_REGULAR_EXPRESSION "${largeInputFailRegex}")
+set (largeInputPassPromptRegex "^Enter a value: " "You entered: abcdefghijklmnopqrstuvwxyz01234567890abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789000:12:34:99999999999999.000000" "^Items read: 4$")
+set (largeInputFailRegex "Error" "Fail" "^Abort trap: \\d+" "^You entered: abcdefghijklmnopqrstuvwxyz01234567890abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz:123456789:0:.*$" "^Items read: 2$")
+set_tests_properties (largeInputTest PROPERTIES
+              PASS_REGULAR_EXPRESSION "${largeInputPassPromptRegex}"
+              REQUIRED_FILES ${helloworld_SOURCE_DIR}/test/fixtures/largeInputTest.txt
+              FAIL_REGULAR_EXPRESSION "${largeInputFailRegex}")

9929707b545ea4e10155dedae3665b251f9ff565
James Cuzella <james.cuzella@lyraphase.com>
2022-01-27 00:39:08 -0700

    CTest Demo: Update helloworld to take input and pass tests

--- helloworld.c
+++ helloworld.c
@@ -3,6 +3,14 @@
 // Copyright 2021 James Cuzella
 
 int main() {
-  printf("Hello My name is James\n");
+  char str[100];
+  int num_i, num_j;
+  int items_read = 0;
+  double dbl_x;
+
+  printf("Enter a value: ");
+  items_read = scanf("%99s %d %d %lf", str, &num_i, &num_j, &dbl_x);
+  printf("\nYou entered: %s:%d:%d:%lf\n", str, num_i, num_j, dbl_x);
+  printf("Items read: %d\n", items_read);
   return 0;
 }
